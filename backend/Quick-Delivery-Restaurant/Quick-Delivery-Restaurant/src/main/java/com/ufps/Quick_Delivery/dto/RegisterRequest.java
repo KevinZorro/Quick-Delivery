f6137882abe74f97483a717fd8a401f741e98155
camilo2212 <juancamilouribemendoza@gmail.com>
2025-10-13 15:31:21 -0500

    mas cambios

---
+++ backend/Quick-Delivery-Restaurant/Quick-Delivery-Restaurant/src/main/java/com/ufps/Quick_Delivery/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.ufps.Quick_Delivery.dto;
+
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String correo;
+    private String password;
+    private String tipoCocina;
+    private String documentosLegales;
+}
