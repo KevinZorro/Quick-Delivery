044c9b38d644eb7b45e98592fb57129936335f0d
Linda Bohorquez <96446503+LindaBohorquez@users.noreply.github.com>
2025-10-21 00:40:51 -0500

    Edge funcional hasta aqui, solo algunas esta bien

--- backend/Quick-Delivery-Edge/Quick-Delivery-Edge/src/main/java/com/ufps/Quick_Delivery/controller/AuthController.java
+++ backend/Quick-Delivery-Edge/Quick-Delivery-Edge/src/main/java/com/ufps/Quick_Delivery/controller/AuthController.java
@@ -1,23 +1,29 @@
 package com.ufps.Quick_Delivery.controller;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.ufps.Quick_Delivery.Auth.AuthRequest;
 import com.ufps.Quick_Delivery.Auth.AuthResponse;
 import com.ufps.Quick_Delivery.model.Usuario;
 import com.ufps.Quick_Delivery.repository.UsuarioRepository;
 import com.ufps.Quick_Delivery.service.JwtService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-
 @RestController
-@RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:4200") // URL del frontend
+@RequestMapping("/auth")
+//@CrossOrigin(origins = "http://localhost:4200") // URL del frontend
 public class AuthController {
 
     @Autowired
@@ -131,8 +137,6 @@ public class AuthController {
                 return "http://localhost:8081";
             case "REPARTIDOR":
                 return "http://localhost:8082";
-            case "ADMIN":
-                return "http://localhost:8084";
             default:
                 return null;
         }
---
+++ backend/Quick-Delivery-Restaurant/Quick-Delivery-Restaurant/src/main/java/com/ufps/Quick_Delivery/dto/CloseAccountRequest.java
@@ -0,0 +1,8 @@
+package com.ufps.Quick_Delivery.dto;
+
+import lombok.Data;
+
+@Data
+public class CloseAccountRequest {
+    private boolean confirm;
+}
