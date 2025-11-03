b40d89678ac29a0c1d9693504902cc654f2c5e7b
Linda Bohorquez <96446503+LindaBohorquez@users.noreply.github.com>
2025-11-02 23:01:01 -0500

    Error Mk

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './features/edge/login.component';
 import { RegisterComponent } from './features/edge/register.component';
-import { InicioComponent } from './features/restaurante/inicio/inicio.component'; // ← VERIFICA ESTA RUTA
+import { InicioComponent } from './features/restaurante/Inicio/inicio.component'; // ← VERIFICA ESTA RUTA
 import { MenuProductoComponent } from './features/restaurante/menu-producto/menu-producto.component'; // ← VERIFICA ESTA RUTA
 import { CerrarComponent } from './features/restaurante/cerrar/cerrar.component';
 import { RegistroRestauranteComponent } from './features/restaurante/features/restaurante/registro-restaurante/registro-restaurante.component';
